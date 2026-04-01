a54d3cc2f70f1d7ffd21b0943a22360784daf9a4
Danie Cunningham <dicunningham@unomaha.edu>
2026-04-01 14:54:11 -0500

    added from_model to convert clingo model symbols into sudoku board dictionary

--- sudoku_board.py
+++ sudoku_board.py
@@ -21,4 +21,13 @@ class Sudoku:
     def from_model(cls, model: clingo.solving.Model) -> "Sudoku":
         sudoku = {}
         # YOUR CODE HERE
+        # get the symbols from the model
+        for atom in model.symbols(shown=True):
+            # loop through each symbol and extract the row, column, and value
+            if atom.name == "sudoku" and len(atom.arguments) == 3:
+                row = atom.arguments[0].number
+                col = atom.arguments[1].number
+                value = atom.arguments[2].number
+                sudoku[(row, col)] = value
+            # add each (row, column) -> value entry to the board dictionary
         return cls(sudoku)
